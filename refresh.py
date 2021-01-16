931f1783dfccb8a7ff7b6566d54a18a166000bad
MaiXiaoMeng <maikaiyang@outlook.com>
2021-01-16 11:28:39 +0800

    feat: 更换脚本启动方式

--- refresh.py
+++ refresh.py
@@ -1,17 +1,14 @@
 '''
 Author: MaiXiaoMeng
-Date: 2020-10-17 12:49:57
+Date: 2021-01-16 11:14:04
 LastEditors: MaiXiaoMeng
-LastEditTime: 2020-11-21 21:29:11
-FilePath: \亚马逊后台数据整理汇总e:\Android\Projects\Python\actions_auto_run\refresh.py
+LastEditTime: 2021-01-16 11:27:11
+FilePath: \actions_auto_run\refresh.py
 '''
-from scripts.tieba_baidu_com import tieba_baidu_com
-from scripts.cloud_189_cn import cloud_189_cn
-from scripts.www_mikugal_com import www_mikugal_com
-from scripts.dagongren_club import dagongren_club
+import os
+
+SCRIPTS_BASE_DIR = './scripts/'
 
 if __name__ == '__main__':
-    www_mikugal_com().run()
-    cloud_189_cn().run()
-    tieba_baidu_com().run()
-    dagongren_club().run()
+    for scripts in os.listdir(SCRIPTS_BASE_DIR):
+        os.system(f'python ./scripts/{scripts}')
